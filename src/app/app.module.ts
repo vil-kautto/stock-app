1b834e33d6f529cc4c980f6b8ea292a53e9070fa
Ville Kautto <ville.kautto@hotmail.fi>
2021-02-17 17:53:04 +0200

    import material modules and add text fields for data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { DatepickerComponent } from './datepicker/datepicker.component'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';  
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackerComponent,
+    DatepickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'fi-FI'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/tracker/tracker.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-tracker',
+  templateUrl: './tracker.component.html',
+  styleUrls: ['./tracker.component.css']
+})
+export class TrackerComponent implements OnInit {
+
+  bullTrend: string = "jaa";
+  tradeVolume: string = "jbb";
+  sma: string = "jcc";
+  sigPrice: string = "jdd";
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
