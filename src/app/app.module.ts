ea4181dd1e6cf2ab83286e78009cf24cf7cc2e31
Ville Kautto <ville.kautto@hotmail.fi>
2021-02-20 13:15:55 +0200

    change application name to more generic one (not just AAPL stock)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// component imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrackerComponent } from './tracker/tracker.component';
 import { DatepickerComponent } from './datepicker/datepicker.component'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// material modules
 import { MatFormFieldModule } from '@angular/material/form-field';  
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core'; 
--- src/app/tracker/tracker.component.ts
+++ src/app/tracker/tracker.component.ts
@@ -19,4 +19,6 @@ export class TrackerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
 }
