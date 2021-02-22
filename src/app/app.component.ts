aef829045e1623dff90800b66642e6da9a439094
Ville Kautto <ville.kautto@hotmail.fi>
2021-02-22 02:59:48 +0200

    add links to external sites

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+/**
+ * this file only contains links and methods to external sites
+ */
 export class AppComponent {
   title = 'my-app';
-   
+  
+  personal:string = "https://github.com/vil-kautto"
+  github:string = "https://github.com/vil-kautto/stock-app";
+  material:string = "https://material.angular.io/";
+  angular:string = "https://angular.io/";
 
+  clickPersonal() {
+    window.location.href = this.personal;
+  }
+
+  clickGithub() {
+    window.location.href = this.github;
+  }
+
+  clickMaterial() {
+    window.location.href = this.material;
+  }
+
+  clickAngular() {
+    window.location.href = this.angular;
+  }
 }
